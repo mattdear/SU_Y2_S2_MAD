d397e4b76517636a1936fdc9e66d16943f507b26
Matt Dear <mrmdear@gmail.com>
2020-04-30 16:29:20 +0100

    I have now got a map up and running on the phone.

--- Mobile_App/app/src/main/java/com/md/placestostay/MainActivity.java
+++ Mobile_App/app/src/main/java/com/md/placestostay/MainActivity.java
@@ -3,12 +3,29 @@ package com.md.placestostay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MapView mv;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
         setContentView(R.layout.activity_main);
+
+        mv = findViewById(R.id.map1);
+
+        mv.setMultiTouchControls(true);
+        mv.getController().setZoom(16.0);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
     }
 }

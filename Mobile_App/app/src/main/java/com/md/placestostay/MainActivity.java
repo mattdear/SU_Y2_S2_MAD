66fe864938cf2c8a511def7128c5e03cbd2829f8
Matt Dear <mrmdear@gmail.com>
2020-04-30 17:01:53 +0100

    I have now started to add the code for a map update based on GPS.

--- Mobile_App/app/src/main/java/com/md/placestostay/MainActivity.java
+++ Mobile_App/app/src/main/java/com/md/placestostay/MainActivity.java
@@ -1,15 +1,20 @@
 package com.md.placestostay;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     MapView mv;
 
@@ -17,15 +22,44 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        //this allows maps to be displayed on screen.
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
-        setContentView(R.layout.activity_main);
+        //this calls the phones location from the GPS and returns it to the app.
+        LocationManager mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         mv = findViewById(R.id.map1);
 
         mv.setMultiTouchControls(true);
         mv.getController().setZoom(16.0);
         mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+
+    }
+
+    public void onLocationChanged(Location newLoc) {
+
+        Toast.makeText
+                (this, "Location=" +
+                        newLoc.getLatitude() + " " +
+                        newLoc.getLongitude(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Provider " + provider +
+                " disabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Provider " + provider +
+                " enabled", Toast.LENGTH_LONG).show();
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        Toast.makeText(this, "Status changed: " + status,
+                Toast.LENGTH_LONG).show();
     }
 }
